b3b72c5831a8166f01d2c686b7059ad4408b24cb
Tejas Bhandirge <tejasbhandirge@Tejass-MacBook-Air.local>
2025-08-20 13:01:58 +0530

    first commit

---
+++ frontendNew/dist/hrms/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WGVJ64K4.mjs";import"./chunk-PKTQTONQ.mjs";import"./chunk-SVHBEZFL.mjs";import"./chunk-QQGZJG6Y.mjs";import"./chunk-XJNXEHJ5.mjs";import"./chunk-OMFD27J4.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
